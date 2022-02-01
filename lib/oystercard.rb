9c35cb1fdd978c2de7449e57c00ff32b5a05f107
Peter O <harmless_goose@icloud.com>
2022-02-01 15:31:20 +0000

    7. & 8. - Can add and deduct from balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,18 @@
 class Oystercard
   attr_reader :balance
+  MAX_LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(value)
+    fail "You can not have more than 90 in your balance" if (balance + value) > MAX_LIMIT
     @balance += value
   end
+
+  def deduct(value)
+    @balance -= value
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,7 +10,24 @@ describe Oystercard do
     it { is_expected.to respond_to(:top_up).with(1).argument }
   
     it "tops up the balance with the argument balance provided" do
-      expect {subject.top_up 500}.to change {subject.balance}.by(500)
+      expect {subject.top_up 5}.to change {subject.balance}.by(5)
     end
+
+    it 'does not allow a total balance of more than 90' do
+      max_limit = Oystercard::MAX_LIMIT
+      subject.top_up(max_limit)
+      expect {subject.top_up 1}.to raise_error "You can not have more than 90 in your balance"
+    end
+  end
+
+  describe "#deduct" do
+    
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it "should deduct value from our balance" do
+      subject.top_up(10)
+      expect { subject.deduct 10}.to change {subject.balance}.by(-10)
+    end
+
   end
 end
